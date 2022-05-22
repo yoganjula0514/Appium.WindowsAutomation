78931c57033d7384b14f17b7021d276862392a71
Yoganjula Reddy Gangarapu <yoganjula.g@hotmail.com>
2022-05-22 22:05:33 +0530

    Added new project to use AppiumServiceBuilder

---
+++ DesktopApplicationAutomation/AppiumServiceBuilder.AutomationNotePad.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppiumServiceBuilderTest.AutomationNotePad.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", @"C:\Windows\System32\notepad.exe");
+
+            var localServiceBuilder = new AppiumServiceBuilder().UsingPort(5555).Build();
+            localServiceBuilder.Start();
+
+            WindowsDriver<WindowsElement> notePadSession = new WindowsDriver<WindowsElement>(localServiceBuilder, appiumOptions);
+            notePadSession.Quit();
+        }
+    }
+}
