505ef4a26757a6f99b8144aaebb18b49c471c069
Yoganjula Reddy Gangarapu <yoganjula.g@hotmail.com>
2022-05-22 21:38:55 +0530

    Get Screenshot and Save in current directory

--- DesktopApplicationAutomation/Appium.AutomateNotePad.ConsoleApp/Program.cs
+++ DesktopApplicationAutomation/Appium.AutomateNotePad.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using System;
+using System.IO;
 
 namespace Appium.AutomateNotePad.ConsoleApp
 {
@@ -10,9 +11,14 @@ namespace Appium.AutomateNotePad.ConsoleApp
         {
             AppiumOptions appiumOptions = new AppiumOptions();
             appiumOptions.AddAdditionalCapability("app", @"C:\Windows\System32\notepad.exe");
+            
             WindowsDriver<WindowsElement> notePadSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+            Console.WriteLine($"Application title : {notePadSession.Title}");
+            notePadSession.Manage().Window.Maximize();
 
-            if(notePadSession == null)
+            var screenshot = notePadSession.GetScreenshot();
+            screenshot.SaveAsFile(Path.Combine(Environment.CurrentDirectory, $"Screenshot{DateTime.Now.ToString("ddMMyyyyhhmmss")}.png"), OpenQA.Selenium.ScreenshotImageFormat.Png);
+            if (notePadSession == null)
             {
                 Console.WriteLine("Application Notepad not started.... Try Again!");
             }
