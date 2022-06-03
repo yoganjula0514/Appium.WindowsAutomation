a94ef0ee93b40359571f4317a2a88fc8127cf80a
Yoganjula Reddy Gangarapu <yoganjula.g@hotmail.com>
2022-06-03 06:16:12 +0530

    Added WPF app and Implemented automation code to enter text in textbox and perform button click operation

--- DesktopApplicationAutomation/Appium.AutomateNotePad.ConsoleApp/Program.cs
+++ DesktopApplicationAutomation/Appium.AutomateNotePad.ConsoleApp/Program.cs
@@ -2,6 +2,9 @@
 using OpenQA.Selenium.Appium.Windows;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using OpenQA.Selenium;
 
 namespace Appium.AutomateNotePad.ConsoleApp
 {
@@ -9,21 +12,44 @@ namespace Appium.AutomateNotePad.ConsoleApp
     {
         static void Main(string[] args)
         {
-            AppiumOptions appiumOptions = new AppiumOptions();
-            appiumOptions.AddAdditionalCapability("app", @"C:\Windows\System32\notepad.exe");
+            var currentDirectory = Directory.GetCurrentDirectory();
             
-            WindowsDriver<WindowsElement> notePadSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
-            Console.WriteLine($"Application title : {notePadSession.Title}");
-            notePadSession.Manage().Window.Maximize();
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", Path.Combine(currentDirectory, @"WPFApp.exe"));
+            
+            WindowsDriver<WindowsElement> wpfAppSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
 
-            var screenshot = notePadSession.GetScreenshot();
-            screenshot.SaveAsFile(Path.Combine(Environment.CurrentDirectory, $"Screenshot{DateTime.Now.ToString("ddMMyyyyhhmmss")}.png"), OpenQA.Selenium.ScreenshotImageFormat.Png);
-            if (notePadSession == null)
-            {
-                Console.WriteLine("Application Notepad not started.... Try Again!");
-            }
+            Console.WriteLine($"Application title : {wpfAppSession.Title}");
+            wpfAppSession.Manage().Window.Maximize();
+            
+            EnterDescriptionContent(wpfAppSession);
+            ClickApplyButton(wpfAppSession);
 
-            notePadSession.Quit();
+            Thread.Sleep(3000);
+
+            ClickApplyButton(wpfAppSession);
+            GetScreenShot(wpfAppSession);
+
+            wpfAppSession.Quit();
+        }
+
+        private static void EnterDescriptionContent(WindowsDriver<WindowsElement> wpfAppSession)
+        {
+            var descriptionTextBox = wpfAppSession.FindElementByAccessibilityId("txtDescription");
+            descriptionTextBox.SendKeys(Keys.Escape);
+            descriptionTextBox.SendKeys("This is dummy Description TextBox");
+        }
+
+        private static void ClickApplyButton(WindowsDriver<WindowsElement> wpfAppSession)
+        {
+            var applyButton = wpfAppSession.FindElementByName("Apply");
+            applyButton.Click();
+        }
+
+        private static void GetScreenShot(WindowsDriver<WindowsElement> wpfAppSession)
+        {
+            var screenShot = wpfAppSession.GetScreenshot();
+            screenShot.SaveAsFile(Path.Combine(Environment.CurrentDirectory, $"Screenshot_{DateTime.Now:ddMMyyyyhhmmss}.png"), OpenQA.Selenium.ScreenshotImageFormat.Png);
         }
     }
 }
---
+++ DesktopApplicationAutomation/Src/WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The Description is : {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //this.WeldCheckBox.IsChecked = this.AssemblyCheckBox.IsChecked = this.PlasmaCheckBox.IsChecked =
+            //    this.LaserCheckBox.IsChecked = this.PurchaseCheckBox.IsChecked = this.LatheCheckBox.IsChecked =
+            //        this.DrillCheckBox.IsChecked = this.FoldCheckBox.IsChecked =
+            //            this.RollCheckBox.IsChecked = this.SawCheckBox.IsChecked = false;
+            this.WeldCheckBox.IsChecked = this.AssemblyCheckBox.IsChecked = this.LatheCheckBox.IsChecked =
+                    this.DrillCheckBox.IsChecked = false;
+        }
+
+        private void CheckBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += ((CheckBox)sender).Content.ToString();
+        }
+
+        private void FinishDropDown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteText == null)
+                return;
+
+            var comboBox = (ComboBox)sender;
+            var comboBoxItem = (ComboBoxItem)comboBox.SelectedValue;
+
+            this.NoteText.Text = comboBoxItem.Content.ToString();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            FinishDropDown_SelectionChanged(this.FinshDropdown, null);
+        }
+
+        private void SupplierNameText_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassText.Text = this.SupplierNameText.Text;
+        }
+    }
+}
